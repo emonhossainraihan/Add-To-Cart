faa83a37f7981601407274f208379a08cb6715d3
emonhossainraihan <mdroy7475@gmail.com>
2020-05-27 23:38:03 +0600

    :bug: Fixing a bug

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({
+    '/products': 'Give all products',
+    '/products/:id': 'Give all information to a specific product',
+    '/orders': 'Give all the orders information',
+    '/orders/:productId': 'Give all information to a specific order',
+  });
 });
 
 module.exports = router;
